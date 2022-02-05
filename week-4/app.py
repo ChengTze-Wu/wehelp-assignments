8a85b45b17a185aa3fb8f9d2ff7beaaaaaa9cb67
ChengTze-Wu <yaoop3050777@gmail.com>
2022-02-05 17:00:08 +0800

    make dif

--- week-4/app.py
+++ week-4/app.py
@@ -8,27 +8,34 @@ app = Flask(__name__)
 
 app.secret_key = "jf8932fhuewifnd9o"
 
-@app.route("/", methods=['GET'])
-def index():
+@app.route("/signup/", methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form["username"]
+        password = request.form["password"]
+        endpoint, message = verify_account(username, password)
+        return redirect(url_for(endpoint=endpoint, message=message))
     return render_template("signin.html")
 
+
+@app.route("/", methods=['GET'])
 @app.route("/signin/", methods=['POST'])
 def signin():
     if request.method == 'POST':
         username = request.form["username"]
         password = request.form["password"]
-        if username == "" or password == "":
-            return redirect(url_for("error", message="請輸入帳號、密碼"))
-        elif verify_account(username, password):
-            session["signin_state"] = 1
-            return redirect(url_for("member"))
-        else:
-            return redirect(url_for("error", message="帳號、或密碼輸入錯誤"))
+        endpoint, message = verify_account(username, password)
+        return redirect(url_for(endpoint=endpoint, message=message))
+    return render_template("signin.html")
 
 def verify_account(username, password):
-    if username == "test" and password == "test":
-        return True
-
+    if username == "" or password == "":
+        return ("error", "請輸入帳號、密碼")
+    elif username == "test" and  password == "test":
+        session["signin_state"] = 1
+        return ("member", None)
+    else:
+        return ("error", "帳號、或密碼輸入錯誤")
 
 @app.route("/signout/", methods=['GET'])
 def signout():
@@ -48,4 +55,4 @@ def error():
     return render_template("error.html", message=message)
 
 if __name__ == '__main__':
-    app.run(port=3000)
+    app.run(debug=True, port=3000)
