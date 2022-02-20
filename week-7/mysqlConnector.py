38988f939694c5fd4edff7df979ac34369a7ac98
ChengTze-Wu <yaoop3050777@gmail.com>
2022-02-20 17:57:17 +0800

    fix

--- week-7/mysqlConnector.py
+++ week-7/mysqlConnector.py
@@ -1,9 +1,10 @@
 import mysql.connector
 from config import DB
 
+cnx = mysql.connector.connect(**DB.dbinfo())
+
 def account_query(username):
     queryData = {}
-    cnx = mysql.connector.connect(pool_name = "pool", pool_size = 10, **DB.dbinfo())
     cursor = cnx.cursor()
     
     query = ("SELECT id, name, username, password FROM member " 
@@ -24,7 +25,7 @@ def account_query(username):
 
 
 def account_create(name, username, passworrd):
-    cnx = mysql.connector.connect(pool_name = "pool", pool_size = 10, **DB.dbinfo())
+    cnx = mysql.connector.connect(**DB.dbinfo())
     cursor = cnx.cursor()
     
     add_member = ("INSERT INTO member (name, username, password) "
@@ -38,7 +39,7 @@ def account_create(name, username, passworrd):
     
 
 def account_name_update(name, username):
-    cnx = mysql.connector.connect(pool_name = "pool", pool_size = 10, **DB.dbinfo())
+    cnx = mysql.connector.connect(**DB.dbinfo())
     
     cursor = cnx.cursor()
     
